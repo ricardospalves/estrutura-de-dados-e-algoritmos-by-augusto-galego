feb26300903a46753b6820e915fb36554391e61d
Ricardo Alves <ricardospalves@gmail.com>
2025-11-05 00:36:06 -0300

    feat(binary-tree/challenge): add algorithm to binary tree level order traversal

---
+++ binary-tree/challenges/binary-tree-level-order-traversal/index.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    response = []
+    queue = collections.deque()
+
+    queue.append(root)
+
+    while queue:
+      level = []
+
+      for i in range(len(queue)):
+        node = queue.popleft()
+
+        if node:
+          level.append(node.val)
+          
+          if node.left:
+            queue.append(node.left)
+          
+          if node.right:
+            queue.append(node.right)
+      
+      if level:
+        response.append(level)
+    
+    return response
